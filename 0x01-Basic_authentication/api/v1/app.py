dcf39b0794d88bf998292875ff2eda7e722c09ed
bakka22 <abobakereltom@gmail.com>
2024-09-03 09:40:21 +0200

    s

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -53,7 +53,7 @@ def unautherized(error) -> str:
 @app.errorhandler(403)
 def forbidden(error) -> str:
     """ unautherized error """
-    return jsonify({"error": "forbidden"}), 403
+    return jsonify({"error": "Forbidden"}), 403
 
 
 if __name__ == "__main__":
