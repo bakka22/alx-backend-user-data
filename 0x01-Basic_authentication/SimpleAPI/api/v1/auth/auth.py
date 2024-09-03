fab5c93e78670ec59d5296e022fa3f893168ff42
bakka22 <abobakereltom@gmail.com>
2024-09-03 09:28:52 +0200

    s

---
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/auth/auth.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+""" manage Authentication """
+from typing import List, TypeVar
+
+
+class Auth():
+    """ authentication class """
+    def require_auth(self, path: str, excluded_paths: List[str]) -> bool:
+        """ check if a path requires authentication """
+        if excluded_paths is None or len(excluded_paths) == 0:
+            return True
+        if path[-1] != "/":
+            path += "/"
+        if path in excluded_paths:
+            return False
+        return True
+
+    def authorization_header(self, request=None) -> str:
+        """ return the value of the header request Authorization """
+        if request is None:
+            return None
+        auth_header = request.headers.get("Authorization", None)
+        return auth_header
+
+    def current_user(self, request=None) -> TypeVar('User'):
+        """ return None """
+        return None
---
+++ 0x01-Basic_authentication/SimpleAPI/api/v1/auth/basic_auth.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+""" Basic Authentication """
+from api.v1.auth.auth import Auth
+from base64 import b64decode
+import base64
+
+
+class BasicAuth(Auth):
+    """ Basic Authentication mechanism class """
+    def extract_base64_authorization_header(self, authorization_header: str) -> str:
+        """ extract base64 Authorization header """
+        if authorization_header is None:
+            return None
+        if type(authorization_header) is not str:
+            return None
+        h_list = authorization_header.split()
+        if h_list[0] != 'Basic':
+            return None
+        return h_list[1]
+
+    def decode_base64_authorization_header(self,
+                                           base64_authorization_header:str) -> str:
+        """ decode base64 authorization header """
+        b64_h = base64_authorization_header
+        if b64_h is None:
+            return None
+        if type(b64_h) != str:
+            return None
+
+        try:
+            decoded = b64decode(b64_h)
+        except base64.binascii.Error:
+            return None
+
+        return decoded.decode('utf-8')
