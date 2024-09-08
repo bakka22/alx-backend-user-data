9f5c61c60a06debf37b4748406e952a7d001e883
bakka22 <abobakereltom@gmail.com>
2024-09-08 14:47:03 +0200

    s

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -3,6 +3,7 @@
 from api.v1.auth.auth import Auth
 import os
 from uuid import uuid4
+from models.user import User
 
 
 class SessionAuth(Auth):
@@ -26,3 +27,13 @@ class SessionAuth(Auth):
         if type(session_id) is not str:
             return None
         return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        """ return User instance based on cookie value """
+        session_id = self.session_cookie(request)
+        if session_id is None:
+            return None
+        usr_id = self.user_id_for_session_id(session_id)
+        if usr_id is None:
+            return None
+        return User.get(usr_id)
