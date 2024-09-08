cbea08932abef0b85ac02be441f9ef8e62306ab7
bakka22 <abobakereltom@gmail.com>
2024-09-08 11:02:33 +0200

    s

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -9,11 +9,19 @@ class SessionAuth(Auth):
     user_id_by_session_id = {}
 
     def create_session(self, user_id: str = None) -> str:
-        """ create a new session """
+        """ create a new session for user """
         if user_id is None:
             return None
         if type(user_id) is not str:
             return None
-        session_id = uuid4()
-        self.__class__.user_id_by_session_id[session_id] = user_id
+        session_id = str(uuid4())
+        self.user_id_by_session_id[session_id] = user_id
         return session_id
+
+    def user_id_for_session_id(self, session_id: str = None) -> str:
+        """ return user_id for a session id """
+        if session_id is None:
+            return None
+        if type(session_id) is not str:
+            return None
+        return self.user_id_by_session_id.get(session_id)
