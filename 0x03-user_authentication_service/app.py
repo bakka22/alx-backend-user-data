e45ac02445b7b2fe71fcc952350c3543991ee2d3
bakka22 <abobakereltom@gmail.com>
2024-09-10 22:28:13 +0200

    s

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 """ flask app module """
-from flask import Flask, jsonify, request
+from flask import Flask, jsonify, request, make_response, abort
 from auth import Auth
 
 
@@ -26,6 +26,22 @@ def users():
     except ValueError:
         return jsonify({"message": "email already registered"}), 400
     return jsonify({"email": f"{email}", "message": "user created"})
+
+
+@app.route("/sessions", methods=["POST"], strict_slashes=False)
+def login():
+    """ create a new user """
+    email = request.form.get("email")
+    password = request.form.get("password")
+    if email is None or password is None:
+        abort(400)
+    if not AUTH.valid_login(email, password):
+        abort(401)
+    payload = {"email": f"{email}", "message": "logged in"}
+    session_id = AUTH.create_session(email)
+    response = make_response(jsonify(payload))
+    response.set_cookie("session_id", session_id)
+    return response
 
 
 if __name__ == "__main__":
